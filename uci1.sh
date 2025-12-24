144c4f6ccfaace31361bfbbd7158617385790741
thaaaru <thaaaru.k@gmail.com>
2025-12-24 13:19:03 +0530

    Update uci1.sh

--- uci1.sh
+++ uci1.sh
@@ -1,34 +1,28 @@
 #!/bin/sh
 
-echo "[+] Configuring mwan3 interfaces (failover mode)"
+echo "[+] Adding firewall zones for mwan3"
 
-# ---------- WAN (SLT - Primary) ----------
-uci set mwan3.wan=interface
-uci set mwan3.wan.enabled='1'
-uci set mwan3.wan.family='ipv4'
-uci set mwan3.wan.track_ip='8.8.8.8'
-uci set mwan3.wan.reliability='1'
-uci set mwan3.wan.count='2'
-uci set mwan3.wan.timeout='2'
-uci set mwan3.wan.interval='5'
-uci set mwan3.wan.metric='10'
-uci set mwan3.wan.weight='1'
+# WAN
+uci set firewall.wan=zone
+uci set firewall.wan.name='wan'
+uci set firewall.wan.input='REJECT'
+uci set firewall.wan.output='ACCEPT'
+uci set firewall.wan.forward='REJECT'
+uci add_list firewall.wan.network='wan'
+uci set firewall.wan.masq='1'
+uci set firewall.wan.mtu_fix='1'
 
-# ---------- WAN2 (Starlink - Backup) ----------
-uci set mwan3.wan2=interface
-uci set mwan3.wan2.enabled='1'
-uci set mwan3.wan2.family='ipv4'
-uci set mwan3.wan2.track_ip='1.1.1.1'
-uci set mwan3.wan2.reliability='1'
-uci set mwan3.wan2.count='2'
-uci set mwan3.wan2.timeout='2'
-uci set mwan3.wan2.interval='5'
-uci set mwan3.wan2.metric='20'
-uci set mwan3.wan2.weight='1'
+# WAN2
+uci set firewall.wan2=zone
+uci set firewall.wan2.name='wan2'
+uci set firewall.wan2.input='REJECT'
+uci set firewall.wan2.output='ACCEPT'
+uci set firewall.wan2.forward='REJECT'
+uci add_list firewall.wan2.network='wan2'
+uci set firewall.wan2.masq='1'
+uci set firewall.wan2.mtu_fix='1'
 
-# ---------- Commit & Restart ----------
-uci commit mwan3
-/etc/init.d/mwan3 restart
+uci commit firewall
+/etc/init.d/firewall restart
 
-echo "[✓] mwan3 interface configuration applied"
-echo "[i] Run: mwan3 status"
+echo "[✓] Firewall zones applied"
