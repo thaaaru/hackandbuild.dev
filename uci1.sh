de42540e77e885cb316ebe355cbf3319b4746052
thaaaru <thaaaru.k@gmail.com>
2025-12-24 13:23:07 +0530

    Fix firewall zone bindings for mwan3

--- uci1.sh
+++ uci1.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
-echo "[+] Adding firewall zones for mwan3"
+echo "[+] Fixing firewall zone bindings for mwan3"
 
-# WAN
-uci set firewall.wan=zone
-uci set firewall.wan.name='wan'
-uci set firewall.wan.input='REJECT'
-uci set firewall.wan.output='ACCEPT'
-uci set firewall.wan.forward='REJECT'
+# Ensure WAN zone has 'wan'
+uci del_list firewall.wan.network='wan' 2>/dev/null
 uci add_list firewall.wan.network='wan'
 uci set firewall.wan.masq='1'
 uci set firewall.wan.mtu_fix='1'
 
-# WAN2
-uci set firewall.wan2=zone
-uci set firewall.wan2.name='wan2'
-uci set firewall.wan2.input='REJECT'
-uci set firewall.wan2.output='ACCEPT'
-uci set firewall.wan2.forward='REJECT'
+# Create WAN2 zone ONLY if missing
+if ! uci get firewall.wan2 >/dev/null 2>&1; then
+    uci add firewall zone
+    uci set firewall.@zone[-1].name='wan2'
+    uci set firewall.@zone[-1].input='REJECT'
+    uci set firewall.@zone[-1].output='ACCEPT'
+    uci set firewall.@zone[-1].forward='REJECT'
+fi
+
+# Bind wan2 network
+uci del_list firewall.wan2.network='wan2' 2>/dev/null
 uci add_list firewall.wan2.network='wan2'
 uci set firewall.wan2.masq='1'
 uci set firewall.wan2.mtu_fix='1'
@@ -25,4 +26,4 @@ uci set firewall.wan2.mtu_fix='1'
 uci commit firewall
 /etc/init.d/firewall restart
 
-echo "[✓] Firewall zones applied"
+echo "[✓] Firewall bindings fixed"
